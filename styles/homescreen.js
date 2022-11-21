221677b0583192738f92b574bfea2cca0d3851a2
Vivianjames <vivianjames2928@gmail.com>
2022-11-21 14:29:45 +0100

    Created some data

---
+++ styles/homescreen.js
@@ -0,0 +1,9 @@
+import { stylesheet } from "react-native";
+
+
+export const styles=stylesheet.create({
+    title:{
+        fontFamily:"Lobster_400Regular",
+        fontSize:40
+    }
+})
---
+++ utilities/AreaView.js
@@ -0,0 +1,9 @@
+import { SafeAreaView,Platform,StatusBar,StyleSheet } from "react-native";
+
+export function SafeAreaView(){
+    return(
+        <SafeAreaView styles={styles}>
+            
+        </SafeAreaView>
+    )
+}
